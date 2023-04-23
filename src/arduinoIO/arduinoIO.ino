4b856a7cf0f1d4998fc2a494a386f6c1530236b2
chris veigl <veigl@technikum-wien.at>
2023-04-23 21:47:50 +0200

    added first low level IO using Arduino

---
+++ src/arduinoIO/arduinoIO.ino
@@ -0,0 +1,54 @@
+
+
+#define FIRST_BUTTON 2
+#define NUM_BUTTONS 6
+
+#define FIRST_LIGHT 10
+#define NUM_LIGHTS 3
+
+void setup() {
+
+  Serial.begin(115200);
+  for (int i=FIRST_BUTTON;i<FIRST_BUTTON+NUM_BUTTONS; i++) {
+    pinMode(i,INPUT_PULLUP);
+  }
+  
+  for (int i=FIRST_LIGHT;i<FIRST_LIGHT+NUM_LIGHTS; i++) {
+    pinMode(i,OUTPUT);
+  }
+
+}
+
+int buttonState[6]={HIGH};
+
+void loop() {
+  static int blinkCount=0;
+  static int blinkPos=0;
+  static int delayTime=1;
+  int tmp;
+  
+  if (++blinkCount>200) {
+    blinkCount=0;
+    digitalWrite(FIRST_LIGHT+blinkPos,LOW);
+    if (++blinkPos>NUM_LIGHTS) blinkPos=0;
+    digitalWrite(FIRST_LIGHT+blinkPos,HIGH);
+  }
+
+  for (int i=0;i<NUM_BUTTONS;i++) {
+
+    if ((tmp=digitalRead(FIRST_BUTTON+i)) != buttonState[i]) {
+      buttonState[i]=tmp;
+      if (tmp==LOW) Serial.print((char) ('0'+i));
+    }
+
+  }
+
+  if (Serial.available()) {
+    int c=Serial.read();
+    if ((c>='0') && (c<='9')) delayTime=c-'0';
+    
+  }
+  
+  delay(delayTime);
+
+}
--- src/flipper.py
+++ src/flipper.py
@@ -2,12 +2,19 @@ import tkinter as tk
 import tkinter.font as tkFont
 from tkinter import *
 from random import randrange
+import pygame
+import serial
+import time
+
 
 screenstate = False
 textcolor="yellow"
 boxcolor="grey"
 backgroundcolor="darkblue"
 maxLetters=5
+
+
+ser = serial.Serial(port="/dev/ttyACM0", baudrate=115200)
 
 
 def toggle_fullscreen(self, event=None):
@@ -64,38 +71,9 @@ def button2_pressed(self, event=None):
     
 
 
-root = tk.Tk()
-
-
-if screenstate == True:
-    fontsize=300
-    xpos=20
-    ypos=200
-    root.attributes('-zoomed', True)  # This just maximizes it so we can see the window. It's nothing to do with fullscreen.
-    root.attributes("-fullscreen", screenstate)
-else:
-    fontsize=30
-    xpos=20
-    ypos=100
-
-tk.Canvas.abs_move = abs_move
-frame = Frame(root)
-frame.pack()
-root.bind("<F11>", toggle_fullscreen)
-root.bind("<Escape>", end_fullscreen)
-root.bind("<F1>", button1_pressed)
-root.bind("<F2>", button2_pressed)
-
-canvas = tk.Canvas(root)
-canvas.pack(fill="both", expand=True)
-canvas.configure(bg=backgroundcolor)
-
-letterfont = tkFont.Font(size = fontsize)
-width = letterfont.measure("0")
-
 def draw(char, x, y, textcolor, boxcolor):
     """Draw one character in a box at the given coordinate"""
-    global canvas,varitext
+    global canvas
 
     text_id = canvas.create_text(x, y, text=char, anchor="nw", font=letterfont, fill=textcolor)
     x0, y0, x1, y1 = canvas.bbox(text_id)
@@ -114,20 +92,49 @@ def animate(string, x, y, textcolor, boxcolor):
         if i==modifyLetter:
             box_id = canvas.create_rectangle(x, y-30, x+width-10, y-10, fill="white", outline="white")
         x=x+width
-    
-    
-
+      
 #    if len(string) > 1:
 #        width = letterfont.measure(string[0]) +10
 #        string = string[1:]
 #        canvas.after(100, animate, canvas, string, x+width, y, textcolor, boxcolor)
 
 
+root = tk.Tk()
+tk.Canvas.abs_move = abs_move
+frame = Frame(root)
+frame.pack()
+canvas = tk.Canvas(root)
+canvas.pack(fill="both", expand=True)
+canvas.configure(bg=backgroundcolor)
+
+if screenstate == True:
+    fontsize=300
+    xpos=20
+    ypos=200
+    root.attributes('-zoomed', True)  # This just maximizes it so we can see the window. It's nothing to do with fullscreen.
+    root.attributes("-fullscreen", screenstate)
+else:
+    fontsize=30
+    xpos=20
+    ypos=100
+    
+letterfont = tkFont.Font(size = fontsize)
+width = letterfont.measure("0")
+
+pygame.mixer.init()
+sound = pygame.mixer.Sound('../sounds/kling1.wav')
+playing = sound.play()
+#while playing.get_busy():
+#    pygame.time.delay(100)
+
+root.bind("<F11>", toggle_fullscreen)
+root.bind("<Escape>", end_fullscreen)
+root.bind("<F1>", button1_pressed)
+root.bind("<F2>", button2_pressed)
 
 text_file = open("worte.txt", "r")
 lines = text_file.readlines()
 text_file.close()
-
 lines[:] = [x.upper() for x in lines if x.strip()]
 # print (lines)
 print (len(lines))
@@ -137,26 +144,33 @@ actword=lines[randrange(len(lines))].rstrip()
 print (actword)
 
 modifyLetter=0   
-
 animate(actword, xpos, ypos, textcolor, boxcolor)
 
+
 myrect = canvas.create_rectangle(10, 10, 20, 20, fill="red", outline="white")
-
 pos=0
-
 def film():
-    global pos, text
+    global pos, text, ser
     canvas.move(myrect,1,0)
+    if (ser.inWaiting() > 0):
+        # read the bytes and convert from binary array to ASCII
+        data_str = ser.read(ser.inWaiting()).decode('ascii') 
+        # print the incoming string without putting a new-line
+        # ('\n') automatically after every print()
+        print(data_str, end='') 
+    
     canvas.after(100, film)
     pos=pos+1
     if pos==50:
         pos=0
         canvas.abs_move(myrect,10,10)
-
+        sound = pygame.mixer.Sound('../sounds/kling2.wav')
+        playing = sound.play()
 
 film()
-root.mainloop()
 
+
+root.mainloop()
 
 
 
