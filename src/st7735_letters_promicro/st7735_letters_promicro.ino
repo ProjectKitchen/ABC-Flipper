eb47c26639f37b345e95db26f4045098edcc98bc
Chris Veigl <veigl@technikum-wien.at>
2023-05-07 00:41:47 +0200

    improved lcd display handling

--- src/st7735_letters_promicro/st7735_letters_promicro.ino
+++ src/st7735_letters_promicro/st7735_letters_promicro.ino
@@ -15,72 +15,98 @@
 #define TFT_RST -1  // Or set to -1 and connect to Arduino RESET pin
 #define TFT_DC 8
 
+#define XPOS 40
+#define YPOS 100
+
+#define MODE_IDLE   0
+#define MODE_LETTER 1
+#define MODE_HIT    2
+
 // For 1.44" and 1.8" TFT with ST7735 use:
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 char text[] = "A";
 uint8_t displayNum = 0;
+int mode = MODE_IDLE;
+uint32_t animTimestamp = 0;
+int animState=0;
 
 void setup(void) {
-
   Serial.begin(9600);
   Serial1.begin(9600);
-
-  pinMode(LED_BUILTIN_TX,INPUT);
-  pinMode(LED_BUILTIN_RX,INPUT);
-
 
   // Use this initializer if using a 1.8" TFT screen:
   tft.initR(INITR_BLACKTAB);  // Init ST7735S chip, black tab
   // tft.initR(INITR_GREENTAB);      // Init ST7735S chip, green tab
-  // tft.setSPISpeed(40000000);
-
-  uint16_t time = millis();
+  tft.setSPISpeed(40000000);
   tft.fillScreen(ST77XX_BLACK);
 
   delay(500);
-  tft.setRotation(1);  // rotate 90 degrees
+  tft.setRotation(3);  // rotate 270 degrees
   tft.setFont(&FreeSansBold24pt7b);
-
   tft.setTextSize(2);
   //tft.setTextSize(13);
+
   tft.fillScreen(ST77XX_BLACK);
-/*
-  for (int i=65;i<255;i++) {
-    text[0]=i;
-    tft.fillScreen(ST77XX_BLACK);
-    tft.setCursor(60, 100);  // 63, 100
-    tft.setTextColor(ST77XX_YELLOW);
-    tft.println(text);
-    delay(300);
-  }
-  */
+  animTimestamp = millis();
+
+  /*
+    for (int i=65;i<255;i++) {
+      text[0]=i;
+      tft.fillScreen(ST77XX_BLACK);
+      tft.setCursor(60, 100);  // 63, 100
+      tft.setTextColor(ST77XX_YELLOW);
+      tft.println(text);
+      delay(300);
+    }
+    */
+
 }
 
 
 void loop() {
   int input = 0;
-  if (Serial.available()) input = Serial.read(); 
+  if (Serial.available()) input = Serial.read();
   else if (Serial1.available()) input = Serial1.read();
-  else {   pinMode(LED_BUILTIN_TX,INPUT);
-          pinMode(LED_BUILTIN_RX,INPUT);
-          return;
+  else {
+    switch (mode) {
+      case MODE_IDLE:
+        if (millis() - animTimestamp >= 500) {
+          animTimestamp = millis();
+          animState++;
+          if (animState == 1) {
+            tft.setCursor(XPOS, YPOS);
+            tft.setTextColor(ST77XX_MAGENTA);
+            tft.println("?");
+          } else {
+            tft.fillScreen(ST77XX_BLACK);
+            animState = 0;
+          }
+        }
+        break;
+      case MODE_LETTER:
+      break;
+      case MODE_HIT:
+      break;
+    }
+    return;
   }
 
-  if ((input>'0') && (input < '6')) {
+  if ((input > '0') && (input < '6')) {
     displayNum = input;
   } else {
     if (displayNum == '1') {
       text[0] = input;
       tft.fillScreen(ST77XX_BLACK);
-      tft.setCursor(60, 100);  // 63, 20
+      tft.setCursor(30, 100);
       tft.setTextColor(ST77XX_YELLOW);
       tft.println(text);
-      displayNum=0;
+      displayNum = 0;
+      mode=MODE_LETTER;
     } else {
-      Serial1.write(displayNum-1);
+      Serial1.write(displayNum - 1);
       Serial1.write((uint8_t)input);
-      displayNum=0;
+      displayNum = 0;
     }
   }
 }
